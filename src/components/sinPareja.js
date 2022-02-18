38dc4ae6765963b30a89acb0687dd4b1f65e9a30
adolivaresl <domioli@gmail.com>
2022-02-18 10:27:34 -0300

    subiendo data de conan

--- src/components/sinPareja.js
+++ src/components/sinPareja.js
@@ -1,11 +1,11 @@
 
 const noVoltear = (primeraTarjeta, segundaTarjeta, tableroBloqueado) => {
-   tableroBloqueado = true;
+   //tableroBloqueado = true;
     // se agrega un tiempo para que vuelva a girarse las cartas
     setTimeout(() => {
         primeraTarjeta.classList.remove('flip');
         segundaTarjeta.classList.remove('flip');
-        tableroBloqueado = false;
+        //tableroBloqueado = false;
     }, 1000);
 }
 
